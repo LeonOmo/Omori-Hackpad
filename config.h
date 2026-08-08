0781910aca6c2bdceabfeec70991a7cfc95ff41e
LeonOmo <lmattersleon@gmail.com>
2026-08-08 16:23:11 +0200

    Fix QMK header import and setup pins

--- config.h
+++ config.h
@@ -1,61 +1,16 @@
-// Copyright 2022 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #pragma once
 
-/**======================
- **    I2C Driver
- *========================**/
+// Direct Pins für die 3 Tasten (SW1, SW2, SW3)
+#define DIRECT_PINS { \
+    { GP26, GP27, GP3 } \
+}
+#define DIRECT_PINS_ACTIVE_LOW
 
-#ifndef I2C_DRIVER
-#    define I2C_DRIVER I2CD1
-#endif
-#ifndef I2C1_SDA_PIN
-#    define I2C1_SDA_PIN D1
-#endif
-#ifndef I2C1_SCL_PIN
-#    define I2C1_SCL_PIN D0
-#endif
+// Rotary Encoder (SW4) Pins
+#define ENCODERS_PAD_A { GP28 }
+#define ENCODERS_PAD_B { GP29 }
+#define ENCODER_RESOLUTION 4
 
-/**======================
- **    SPI Driver
- *========================**/
-#ifndef SPI_DRIVER
-#    define SPI_DRIVER SPID0
-#endif
-#ifndef SPI_SCK_PIN
-#    define SPI_SCK_PIN B1
-#endif
-#ifndef SPI_MISO_PIN
-#    define SPI_MISO_PIN B3
-#endif
-#ifndef SPI_MOSI_PIN
-#    define SPI_MOSI_PIN B2
-#endif
-
-/**======================
- **      UART Driver
- *========================**/
-
-#ifndef UART_DRIVER
-#    define UART_DRIVER SIOD0
-#endif
-
-#ifndef UART_TX_PIN
-#    define UART_TX_PIN D3
-#endif
-
-#ifndef UART_RX_PIN
-#    define UART_RX_PIN D2
-#endif
-
-/**======================
- **    Double-tap
- *========================**/
-
-#ifndef RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#    define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#endif
-#ifndef RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT
-#    define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
-#endif
+// OLED Display (J1) I2C Pins
+#define I2C1_SDA_PIN GP1
+#define I2C1_SCL_PIN GP2
